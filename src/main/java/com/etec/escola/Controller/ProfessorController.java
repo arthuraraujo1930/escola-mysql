719559458796aba374e331e01ff2239c14e396cb
arthuraraujo1930 <arthurenzo0223@gmail.com>
2025-09-03 16:59:43 -0300

    aula-java-03-09-25

--- src/main/java/com/etec/escola/Controller/ProfessorController.java
+++ src/main/java/com/etec/escola/Controller/ProfessorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("professsor")
+@RequestMapping("/professsor")
 public class ProfessorController {
 
     private final ProfessorService professorService;
