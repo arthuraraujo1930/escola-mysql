d801fe95599893bdfbda88baee9da447cb5e7669
arthuraraujo1930 <arthurenzo0223@gmail.com>
2025-09-10 17:21:14 -0300

    aula do dia 10-09-25

--- src/main/java/com/etec/escola/Controller/AuthController.java
+++ src/main/java/com/etec/escola/Controller/AuthController.java
@@ -1,4 +1,33 @@
 package com.etec.escola.Controller;
 
+
+import com.etec.escola.Models.Usuario;
+import com.etec.escola.Service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*") //permite acesso do browser
 public class AuthController {
+
+    private final UsuarioService service;
+
+    public AuthController(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public Usuario register(@RequestBody Usuario usuario){
+        return service.cadastrarNovoUsuario(usuario);
+    }
+
+    @PostMapping("/login")
+    public String login(Usuario usuario){
+        Usuario user = service.fazerLogin(usuario.getUsername(), usuario.getSenha());
+        if (user != null){
+            return "Login realizado com sucesso"+ user.getUsername();
+        }
+        return "Usuario ou senha inválidos";
+    }
+
 }
--- src/main/java/com/etec/escola/Service/UsuarioService.java
+++ src/main/java/com/etec/escola/Service/UsuarioService.java
@@ -1,4 +1,26 @@
 package com.etec.escola.Service;
 
+import com.etec.escola.Interface.AuthUsuarioRepository;
+import com.etec.escola.Models.Usuario;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+    private final AuthUsuarioRepository userRepo;
+
+
+    public UsuarioService(AuthUsuarioRepository usuarioRepository) {
+        this.userRepo = usuarioRepository;
+    }
+
+    public Usuario fazerLogin(String username, String senha){
+        return userRepo.findByUsernameAndSenha(username, senha).orElse(null);
+    }
+
+
+    public Usuario cadastrarNovoUsuario(Usuario user){
+        return userRepo.save(user);
+    }
+
+
 }
